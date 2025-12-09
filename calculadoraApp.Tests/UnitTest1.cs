38834fa4a986ad9ea937c5c0761f3196b431257d
DavidDu2002 <campoverderamos@gmail.com>
2025-12-09 11:04:12 -0500

    ProyectoCalculadora

---
+++ calculadoraApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+namespace calculadoraApp.Tests
+{   
+    public class CalculadoraTest
+    {
+        [Fact]
+        public void Sumar_DosNumeros_VerificaSumaCorrect()
+
+        {
+            var calc = new calculadoraApp.calculadora();
+            var resultado = calc.Sumar(3, 3);
+            Assert.Equal(5, resultado);
+        }
+    }
+
+}
+
